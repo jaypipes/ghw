7a12525e93e78ae265ba8a4156c5e3e5b69785d6
Jay Pipes <jaypipes@gmail.com>
2017-05-29 11:31:15 -0400

    linux: add unit test for supported page sizes

--- memory_test.go
+++ memory_test.go
@@ -19,3 +19,14 @@ func TestTotalUsableBytes(t *testing.T) {
         t.Errorf("Expected >0 total usable memory, got %d", tpb)
     }
 }
+
+func TestSupportedPageSizes(t *testing.T) {
+    sps := memSupportedPageSizes()
+
+    if sps == nil {
+        t.Errorf("Expected non-nil supported page sizes, but got nil")
+    }
+    if len(sps) == 0 {
+        t.Errorf("Expected >0 supported page sizes, but got 0.")
+    }
+}
