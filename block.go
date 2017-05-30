06e57d4ac517428b4d9f24148ffce598a4762d70
Jay Pipes <jaypipes@gmail.com>
2017-05-30 18:34:46 -0400

    Stringify ghw.Disk

--- block.go
+++ block.go
@@ -45,9 +45,39 @@ func (i *BlockInfo) String() string {
     if i.TotalPhysicalBytes > 0 {
         tpb := i.TotalPhysicalBytes
         unit, unitStr := unitWithString(int64(tpb))
-        tpb = uint64(math.Ceil(float64(i.TotalPhysicalBytes) / float64(unit)))
+        tpb = uint64(math.Ceil(float64(tpb) / float64(unit)))
         tpbs = fmt.Sprintf("%d%s", tpb, unitStr)
     }
-    return fmt.Sprintf("block storage (%s physical)", tpbs)
+    dplural := "disks"
+    if len(i.Disks) == 1 {
+        dplural = "disk"
+    }
+    return fmt.Sprintf("block storage (%d %s, %s physical storage)",
+                       len(i.Disks), dplural, tpbs)
 }
 
+func (d *Disk) String() string {
+    vendor := ""
+    if d.Vendor != "" {
+        vendor =  "  " + d.Vendor
+    }
+    serial := ""
+    if d.SerialNumber != "" {
+        serial = " - SN #" + d.SerialNumber
+    }
+    sizeStr := "unknown"
+    if d.SizeBytes > 0 {
+        size := d.SizeBytes
+        unit, unitStr := unitWithString(int64(size))
+        size = uint64(math.Ceil(float64(size) / float64(unit)))
+        sizeStr = fmt.Sprintf("%d%s", size, unitStr)
+    }
+    return fmt.Sprintf(
+        "/dev/%s (%s) [%s]%s%s",
+        d.Name,
+        sizeStr,
+        d.BusType,
+        vendor,
+        serial,
+    )
+}
