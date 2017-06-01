760d8a75e3322a440b7962a5b0387b4853e9d326
Jay Pipes <jaypipes@gmail.com>
2017-06-01 16:34:04 -0400

    Add top-level main_test.go for cpu

--- main_test.go
+++ main_test.go
@@ -29,6 +29,21 @@ func TestHost(t *testing.T) {
         t.Fatalf("Expected >0 total usable memory, but got %d", tub)
     }
 
+    cpu := host.CPU
+    if cpu == nil {
+        t.Fatalf("Expected non-nil CPU, but got nil")
+    }
+
+    cores := cpu.TotalCores
+    if cores < 1 }
+        t.Fatalf("Expected >0 total cores, but got %d", cores)
+    }
+
+    threads := cpu.TotalThreads
+    if threads < 1 }
+        t.Fatalf("Expected >0 total threads, but got %d", threads)
+    }
+
     block := host.Block
     if block == nil {
         t.Fatalf("Expected non-nil Block but got nil.")
