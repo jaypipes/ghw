2a413f1ab65a9b99bc2335adaeb7edaa2098161a
Jay Pipes <jaypipes@gmail.com>
2019-12-26 16:58:45 -0500

    start on snapshotting Linux sysfs/procfs
    Issue #66

---
+++ cmd/ghw-snapshot/main.go
@@ -0,0 +1,215 @@
+//
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+package main
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// version of application at compile time (-X 'main.version=$(VERSION)').
+	version = "(Unknown Version)"
+	// buildHash GIT hash of application at compile time (-X 'main.buildHash=$(GITCOMMIT)').
+	buildHash = "No Git-hash Provided."
+	// buildDate of application at compile time (-X 'main.buildDate=$(BUILDDATE)').
+	buildDate = "No Build Date Provided."
+	// show debug output
+	debug = false
+	// output filepath to save snapshot to
+	outPath string
+)
+
+type FileInfoFilter func(os.FileInfo) (bool, error)
+
+var (
+	pseudoFiles = []string{
+		"/proc/cpuinfo",
+		"/proc/meminfo",
+	}
+)
+
+func regularFileFilter(fi os.FileInfo) (bool, error) {
+	return fi.Mode().IsRegular(), nil
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ghw-snapshot",
+	Short: "ghw-snapshot - Snapshot filesystem containing system information.",
+	RunE:  execute,
+}
+
+func trace(msg string, args ...interface{}) {
+	if !debug {
+		return
+	}
+	fmt.Printf(msg, args...)
+}
+
+func systemFingerprint() string {
+	return "jaypipes"
+}
+
+func defaultOutPath() string {
+	fp := systemFingerprint()
+	return fmt.Sprintf("%s-%s-%s.tar.gz", runtime.GOOS, runtime.GOARCH, fp)
+}
+
+func execute(cmd *cobra.Command, args []string) error {
+	// Attempting to tar up pseudofiles like /proc/cpuinfo is an exercise in
+	// futility. Instead, it is necessary to build a directory structure in a
+	// tmpdir and create actual files with copies of the pseudofile contents
+	scratchDir, err := ioutil.TempDir("", "ghw-snapshot")
+	if err != nil {
+		return err
+	}
+
+	var createPaths = []string{
+		"proc",
+		"sys/block",
+	}
+
+	for _, path := range createPaths {
+		if err = os.MkdirAll(filepath.Join(scratchDir, path), os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	if err = createProcfiles(scratchDir); err != nil {
+		return err
+	}
+	return doSnapshot(scratchDir)
+}
+
+func createProcfiles(buildDir string) error {
+	var createPaths = []string{
+		"/proc/cpuinfo",
+		"/proc/meminfo",
+	}
+
+	for _, path := range createPaths {
+		buf, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		targetPath := filepath.Join(buildDir, path)
+		f, err := os.Create(targetPath)
+		if err != nil {
+			return err
+		}
+		if _, err = f.Write(buf); err != nil {
+			return err
+		}
+		f.Close()
+	}
+	return nil
+}
+
+func doSnapshot(buildDir string) error {
+	if outPath == "" {
+		outPath = defaultOutPath()
+		trace("using default output filepath %s\n", outPath)
+	}
+
+	var f *os.File
+	var err error
+
+	if _, err = os.Stat(outPath); os.IsNotExist(err) {
+		if f, err = os.Create(outPath); err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	} else {
+		f, err := os.OpenFile(outPath, os.O_WRONLY, 0600)
+		if err != nil {
+			return err
+		}
+		fs, err := f.Stat()
+		if err != nil {
+			return err
+		}
+		if fs.Size() > 0 {
+			return fmt.Errorf("File %s already exists and is of size >0", outPath)
+		}
+	}
+	defer f.Close()
+
+	gzw := gzip.NewWriter(f)
+	defer gzw.Close()
+
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	return createSnapshot(tw, buildDir)
+}
+
+func main() {
+	rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(
+		&outPath,
+		"out", "o",
+		outPath,
+		"Path to place snapshot. Defaults to file in current directory with name $OS-$ARCH-$HASHSYSTEMNAME.tar.gz",
+	)
+	rootCmd.PersistentFlags().BoolVarP(
+		&debug, "debug", "d", false, "Enable or disable debug mode",
+	)
+}
+
+func createSnapshot(tw *tar.Writer, buildDir string) error {
+	return filepath.Walk(buildDir, func(path string, fi os.FileInfo, err error) error {
+		if path == buildDir {
+			return nil
+		}
+		var link string
+
+		if fi.Mode()&os.ModeSymlink != 0 {
+			trace("processing symlink %s\n", path)
+			link, err = os.Readlink(path)
+			if err != nil {
+				return err
+			}
+		}
+
+		hdr, err := tar.FileInfoHeader(fi, link)
+		if err != nil {
+			return err
+		}
+		hdr.Name = strings.TrimPrefix(strings.TrimPrefix(path, buildDir), string(os.PathSeparator))
+
+		if err = tw.WriteHeader(hdr); err != nil {
+			return err
+		}
+
+		switch hdr.Typeflag {
+		case tar.TypeReg, tar.TypeRegA:
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			if _, err = io.Copy(tw, f); err != nil {
+				return err
+			}
+			f.Close()
+		}
+		return nil
+	})
+}
