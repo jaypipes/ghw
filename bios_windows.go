0f60369ae1b130a89f4d5587bae51be64005ce3a
Riccardo Re <riccardo.re@clevermind.cloud>
2020-02-05 10:03:57 +0100

    Windows partial support - Baseboard, BIOS, chassis, product
    First commit

---
+++ bios_windows.go
@@ -0,0 +1,29 @@
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+package ghw
+
+import "github.com/StackExchange/wmi"
+
+type cIM_BIOSElement struct {
+	InstallDate  string
+	Manufacturer string
+	Version      string
+}
+
+func (ctx *context) biosFillInfo(info *BIOSInfo) error {
+	// Getting data from WMI
+	var win32BIOSDescriptions []cIM_BIOSElement
+	q1 := wmi.CreateQuery(&win32BIOSDescriptions, "")
+	if err := wmi.Query(q1, &win32BIOSDescriptions); err != nil {
+		return err
+	}
+	if len(win32BIOSDescriptions) > 0 {
+		info.Vendor = win32BIOSDescriptions[0].Manufacturer
+		info.Version = win32BIOSDescriptions[0].Version
+		info.Date = win32BIOSDescriptions[0].InstallDate
+	}
+	return nil
+}
