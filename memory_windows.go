63b6f3dca7295f79f251309900be2f0ea6176692
Jay Pipes <jaypipes@gmail.com>
2020-05-22 14:56:08 -0400

    use all pointer types for receiving fields in WMI
    Apparently WMI will return `<nil>` type even for integer fields like
    DefaultBlockSize :(
    
    Issue #189

--- memory_windows.go
+++ memory_windows.go
@@ -13,26 +13,26 @@ import (
 const wqlOperatingSystem = "SELECT TotalVisibleMemorySize FROM Win32_OperatingSystem"
 
 type win32OperatingSystem struct {
-	TotalVisibleMemorySize uint64
+	TotalVisibleMemorySize *uint64
 }
 
 const wqlPhysicalMemory = "SELECT BankLabel, Capacity, DataWidth, Description, DeviceLocator, Manufacturer, Model, Name, PartNumber, PositionInRow, SerialNumber, Speed, Tag, TotalWidth FROM Win32_PhysicalMemory"
 
 type win32PhysicalMemory struct {
 	BankLabel     *string
-	Capacity      uint64
-	DataWidth     uint16
+	Capacity      *uint64
+	DataWidth     *uint16
 	Description   *string
 	DeviceLocator *string
 	Manufacturer  *string
 	Model         *string
 	Name          *string
 	PartNumber    *string
-	PositionInRow uint32
+	PositionInRow *uint32
 	SerialNumber  *string
-	Speed         uint32
+	Speed         *uint32
 	Tag           *string
-	TotalWidth    uint16
+	TotalWidth    *uint16
 }
 
 func (ctx *context) memFillInfo(info *MemoryInfo) error {
@@ -49,12 +49,12 @@ func (ctx *context) memFillInfo(info *MemoryInfo) error {
 	var totalPhysicalBytes uint64
 	info.Modules = make([]*MemoryModule, 0, len(win32MemDescriptions))
 	for _, description := range win32MemDescriptions {
-		totalPhysicalBytes += description.Capacity
+		totalPhysicalBytes += *description.Capacity
 		info.Modules = append(info.Modules, &MemoryModule{
 			Label:        *description.BankLabel,
 			Location:     *description.DeviceLocator,
 			SerialNumber: *description.SerialNumber,
-			SizeBytes:    int64(description.Capacity),
+			SizeBytes:    int64(*description.Capacity),
 			Vendor:       *description.Manufacturer,
 		})
 	}
@@ -62,7 +62,7 @@ func (ctx *context) memFillInfo(info *MemoryInfo) error {
 	for _, description := range win32OSDescriptions {
 		// TotalVisibleMemorySize is the amount of memory available for us by
 		// the operating system **in Kilobytes**
-		totalUsableBytes += description.TotalVisibleMemorySize * uint64(KB)
+		totalUsableBytes += *description.TotalVisibleMemorySize * uint64(KB)
 	}
 	info.TotalUsableBytes = int64(totalUsableBytes)
 	info.TotalPhysicalBytes = int64(totalPhysicalBytes)
