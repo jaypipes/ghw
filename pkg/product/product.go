1a9fae71d9ffda99c99afdab8b96e0fba708945d
Francesco Romani <fromani@redhat.com>
2020-12-11 09:50:47 +0100

    pkg: add support to consume snapshots
    Add support to allow `ghw` read from snapshots (created with `ghw-snapshot`) using
    environment variables or programmatically.
    This is meant to be used mostly in the testsuite, but can be useful for
    users in general.
    
    Added environment variables (and counterpart in golang pkgs):
    - `GHW_SNAPSHOT_PATH` let users specify a snapshot
       that `ghw` will automatically consume.
    - `GHW_SNAPSHOT_ROOT` let users specify the directory
       on which the snapshot should be unpacked.
    - `GHW_SNAPSHOT_EXCLUSIVE` is relevant iff `GHW_SNAPSHOT_ROOT` is given.
       Tells `ghw` that the directory is meant only to contain the given snapshot,
       thus `ghw` will *not* attempt to unpack it
       (and will go ahead silently!) unless the directory is empty.
    - `GHW_SNAPSHOT_PRESERVE` if set, `ghw` will *not*
       clean up the unpacked snapshot once done, leaving it into the system.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/product/product.go
+++ pkg/product/product.go
@@ -75,7 +75,7 @@ func (i *Info) String() string {
 func New(opts ...*option.Option) (*Info, error) {
 	ctx := context.New(opts...)
 	info := &Info{ctx: ctx}
-	if err := info.load(); err != nil {
+	if err := ctx.Do(info.load); err != nil {
 		return nil, err
 	}
 	return info, nil
