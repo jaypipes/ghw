906117d3413d138b63d7767cea788a83054aa310
Francesco Romani <fromani@redhat.com>
2021-06-14 09:22:51 +0200

    snapshot: mark OS-specific code as so
    In the snapshot package we have code to clone relevant filesystem
    information. This is by its nature OS-specific, but because a oversight
    we previously didn't marked as so.
    
    This became evident while trying to enable darwin and windows specific
    CI lanes (see https://github.com/jaypipes/ghw/pull/259).
    
    With this patch we now correctly declare the clonetree code as OS
    specific. No intended changes in behaviour on linux.
    
    As final note, this change also highlight we will need snapshot-specific
    code for darwin/windows (or a similar machinery), which we postpone
    for lack of capacity and access to relevant platforms.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ pkg/snapshot/clonetree_stub.go
@@ -0,0 +1,34 @@
+// +build !linux
+//
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+package snapshot
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func setupScratchDir(scratchDir string) error {
+	return nil
+}
+
+func ExpectedCloneStaticContent() []string {
+	return []string{}
+}
+
+func ExpectedCloneGPUContent() []string {
+	return []string{}
+}
+
+func ExpectedCloneNetContent() []string {
+	return []string{}
+}
+
+func ExpectedClonePCIContent() []string {
+	return []string{}
+}
