18c1db40ece60024c8d53b35d18824396e483274
Jay Pipes <jaypipes@gmail.com>
2021-09-27 09:08:43 -0400

    allow passing in existing Context as an Option
    This patch simplifies some internal-only code that was added to ghw to
    prevent re-creating Context objects inefficiently during snapshot
    creation. We add a `context.WithContext` function that returns an
    `option.Option` containing a pre-existing `Context` struct pointer,
    embedded in the `Option` as a raw `interface{}` to get around package
    recursive import issues.
    
    Now, when packages like `pkg/gpu` or `pkg/pci` need to load the
    `pkg/topology` package, they call
    `topology.New(context.WithContext(ctx))`, passing in the
    already-existing `Context` object. This preserves the original `New`
    function signature without adding a new `NewWithContext` function
    signature to all packages.
    
    Alternative to #278
    
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

--- pkg/pci/pci.go
+++ pkg/pci/pci.go
@@ -155,27 +155,29 @@ func (i *Info) String() string {
 // New returns a pointer to an Info struct that contains information about the
 // PCI devices on the host system
 func New(opts ...*option.Option) (*Info, error) {
-	return NewWithContext(context.New(opts...))
-}
-
-// NewWithContext returns a pointer to an Info struct that contains information about
-// the PCI devices on the host system. Use this function when you want to consume
-// the topology package from another package (e.g. gpu)
-func NewWithContext(ctx *context.Context) (*Info, error) {
+	merged := option.Merge(opts...)
+	ctx := context.New(merged)
 	// by default we don't report NUMA information;
 	// we will only if are sure we are running on NUMA architecture
-	arch := topology.ARCHITECTURE_SMP
-	topo, err := topology.NewWithContext(ctx)
-	if err == nil {
-		arch = topo.Architecture
-	} else {
-		ctx.Warn("error detecting system topology: %v", err)
-	}
 	info := &Info{
-		arch: arch,
+		arch: topology.ARCHITECTURE_SMP,
 		ctx:  ctx,
 	}
-	if err := ctx.Do(info.load); err != nil {
+	// we do this trick because we need to make sure ctx.Setup() gets
+	// a chance to run before any subordinate package is created reusing
+	// our context.
+	if err := ctx.Do(func() error {
+		topo, err := topology.New(context.WithContext(ctx))
+		if err == nil {
+			info.arch = topo.Architecture
+		} else {
+			ctx.Warn("error detecting system topology: %v", err)
+		}
+		if context.Exists(merged) {
+			return info.load()
+		}
+		return ctx.Do(info.load)
+	}); err != nil {
 		return nil, err
 	}
 	return info, nil
