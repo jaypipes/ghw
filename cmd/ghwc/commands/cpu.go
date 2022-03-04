67317b240ae49d812ac42d91cb66e68b6917abe9
Xingyou Chen <rockrush@rockwork.org>
2022-03-04 16:33:33 +0800

    Add aarch64 and RISCV64 support
    Signed-off-by: Xingyou Chen <rockrush@rockwork.org>

--- cmd/ghwc/commands/cpu.go
+++ cmd/ghwc/commands/cpu.go
@@ -43,12 +43,12 @@ func showCPU(cmd *cobra.Command, args []string) error {
 				// pretty-print the (large) block of capability strings into rows
 				// of 6 capability strings
 				rows := int(math.Ceil(float64(len(proc.Capabilities)) / float64(6)))
-				for row := 1; row < rows; row = row + 1 {
-					rowStart := (row * 6) - 1
+				for row := 0; row < rows; row = row + 1 {
+					rowStart := (row * 6)
 					rowEnd := int(math.Min(float64(rowStart+6), float64(len(proc.Capabilities))))
 					rowElems := proc.Capabilities[rowStart:rowEnd]
 					capStr := strings.Join(rowElems, " ")
-					if row == 1 {
+					if row == 0 {
 						fmt.Printf("  capabilities: [%s\n", capStr)
 					} else if rowEnd < len(proc.Capabilities) {
 						fmt.Printf("                 %s\n", capStr)
