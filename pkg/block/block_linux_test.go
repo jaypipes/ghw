d314f334ae59b6ea0b8b5f8c2ab55add2123f437
Itxaka <igarcia@suse.com>
2022-03-17 14:10:51 +0100

    Get partition type for unmounted partitions
    If a partition is unmounted our current way of getting the partition
    type wont work, as it only checks for mounted partitions.
    
    This patch makes it check in udev if the partition type is empty
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- pkg/block/block_linux_test.go
+++ pkg/block/block_linux_test.go
@@ -251,3 +251,34 @@ func TestDiskPartLabel(t *testing.T) {
 		t.Fatalf("Got label %s, but expected %s label", label, util.UNKNOWN)
 	}
 }
+
+func TestDiskTypeUdev(t *testing.T) {
+	if _, ok := os.LookupEnv("GHW_TESTING_SKIP_BLOCK"); ok {
+		t.Skip("Skipping block tests.")
+	}
+	baseDir, _ := ioutil.TempDir("", "test")
+	defer os.RemoveAll(baseDir)
+	ctx := context.New()
+	ctx.Chroot = baseDir
+	paths := linuxpath.New(ctx)
+	fsType := "ext4"
+
+	_ = os.MkdirAll(paths.SysBlock, 0755)
+	_ = os.MkdirAll(paths.RunUdevData, 0755)
+
+	// Emulate a disk with one partition with label TEST_LABEL_GHW
+	_ = os.Mkdir(filepath.Join(paths.SysBlock, "sda"), 0755)
+	_ = os.Mkdir(filepath.Join(paths.SysBlock, "sda", "sda1"), 0755)
+	_ = ioutil.WriteFile(filepath.Join(paths.SysBlock, "sda", "sda1", "dev"), []byte("259:0\n"), 0644)
+	_ = ioutil.WriteFile(filepath.Join(paths.RunUdevData, "b259:0"), []byte(fmt.Sprintf("E:ID_FS_TYPE=%s\n", fsType)), 0644)
+	fs := diskPartTypeUdev(paths, "sda", "sda1")
+	if fs != fsType {
+		t.Fatalf("Got fs %s but expected %s", fs, fsType)
+	}
+
+	// Check empty fs if not found
+	fs = diskPartTypeUdev(paths, "sda", "sda2")
+	if fs != util.UNKNOWN {
+		t.Fatalf("Got label %s, but expected %s label", fs, util.UNKNOWN)
+	}
+}
