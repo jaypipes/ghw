82590335a6eacb1cc50f91abc2810abd37079fbf
Jay Pipes <jaypipes@gmail.com>
2025-08-01 15:07:34 -0400

    make pkg/accelerator.Info() return empty not error
    Simply makes the Windows Accelerator.Info() return an empty Info struct
    instead of erroring out with a not-implemented error.
    
    Issue #418
    
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

--- pkg/accelerator/accelerator_windows.go
+++ pkg/accelerator/accelerator_windows.go
@@ -1,6 +1,3 @@
-//go:build !linux && !windows
-// +build !linux,!windows
-
 // Use and distribution licensed under the Apache license version 2.
 //
 // See the COPYING file in the root project directory for full text.
@@ -8,12 +5,7 @@
 
 package accelerator
 
-import (
-	"runtime"
-
-	"github.com/pkg/errors"
-)
-
 func (i *Info) load() error {
-	return errors.New("accelerator.Info.load not implemented on " + runtime.GOOS)
+	i.Devices = []*AcceleratorDevice{}
+	return nil
 }
